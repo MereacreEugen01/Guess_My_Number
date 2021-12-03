8bbdc66942d7c82a0bbd230ea0af5e70c5e819a5
LenziFilippo04 <lenzi.filippo@studenti-ittfedifermi.edu.it>
2021-12-03 13:02:08 +0100

    Creati vari onClick, interazioni tra activity e onClick in Game_Activity da errore

--- app/src/main/java/com/example/guess_my_number/Game_Activity.java
+++ app/src/main/java/com/example/guess_my_number/Game_Activity.java
@@ -3,12 +3,66 @@ package com.example.guess_my_number;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Game_Activity extends AppCompatActivity {
+
+    private int counter = 0;
+    private TextView rifTriesCounter;
+    private TextView rifGuessView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        this.rifTriesCounter = (TextView) this.findViewById(R.id.TriesCounter);
+        this.rifGuessView = (TextView) this.findViewById(R.id.GuessView);
+    }
+
+    public void onClickGuess(View arg0){
+        //Controllo numero
+        counter++;
+        this.rifTriesCounter.setText(counter);
+    }
+
+    public void onClick1(View arg0){
+        this.rifGuessView.append("1");
+    }
+
+    public void onClick2(View arg0){
+        this.rifGuessView.append("2");
+    }
+
+    public void onClick3(View arg0){
+        this.rifGuessView.append("3");
+    }
+
+    public void onClick4(View arg0){
+        this.rifGuessView.append("4");
+    }
+
+    public void onClick5(View arg0){
+        this.rifGuessView.append("5");
+    }
+
+    public void onClick6(View arg0){
+        this.rifGuessView.append("6");
+    }
+
+    public void onClick7(View arg0){
+        this.rifGuessView.append("7");
+    }
+
+    public void onClick8(View arg0){
+        this.rifGuessView.append("8");
+    }
+
+    public void onClick9(View arg0){
+        this.rifGuessView.append("9");
+    }
+
+    public void onClick0(View arg0){
+        this.rifGuessView.append("0");
     }
 }
--- app/src/main/java/com/example/guess_my_number/MainActivity.java
+++ app/src/main/java/com/example/guess_my_number/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.guess_my_number;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("Ciao");
-        System.out.println("Ciao di risposta");
     }
+    public void onClickGame(View arg0) {
+        // definisco l'intenzione
+        Intent game = new Intent(MainActivity.this,Game_Activity.class);
+        // passo all'attivazione dell'activity Game_Activity.java
+        startActivity(game);
+    }
+
+    public void onClickScore(View arg0) {
+        // definisco l'intenzione
+        Intent score = new Intent(MainActivity.this,Score_Activity.class);
+        // passo all'attivazione dell'activity Score_Activity.java
+        startActivity(score);
+    }
+
 }
